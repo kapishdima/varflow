c46e0403234f2d4aa5dd3710607d14389924d311
DimaDevelopment <dima.development@wandry.com.ua>
2024-06-14 02:00:03 +0300

    fixed console texts

--- resources/views/components/console.blade.php
+++ resources/views/components/console.blade.php
@@ -8,8 +8,8 @@
         <div style="white-space: pre;" class="console__text">
             <p class="console__font console__font--orange" style="display:none;">@lang('default.console_text_1')<span
                     class='console__font console__font--green'> VarFlow</span></p>
-            <p class="console__font console__font--white" style="display:none;">@lang('default.console_text_2') <span
-                    class="console__font console__font--orange">@lang('default.console_text_3') </span></p>
+            <p class="console__font console__font--white" style="display:none;">@lang('default.console_text_2')</p>
+            <p class="console__font console__font--white" style="display:none;">@lang('default.console_text_3')</p>
             <p class='console__font console__font--green' style="display:none;">@lang('default.console_text_4') </p>
             <p class="console__font console__font--white" style="display:none;">@lang('default.console_text_5')</p>
         </div>
