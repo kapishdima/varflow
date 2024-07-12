a73c8e5b4d5e9234f5b4a97f07711b5e206ec512
DimaDevelopment <dima.development@wandry.com.ua>
2024-07-12 13:11:21 +0300

    added GTM

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,19 @@
 
     {{ useFrontStyles() }}
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MDNWBCCPMR"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-MDNWBCCPMR');
+    </script>
+
 </head>
 
 <body id="{{ $page ?? '' }}">
